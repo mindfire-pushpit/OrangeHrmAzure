d81e90d6356abdfc7d551baa763052ca02ee6a04
mindifire-pushpit <107248924+mindfire-pushpit@users.noreply.github.com>
2024-11-22 19:51:52 +0530

    second commit

---
+++ tests/test01.spec.js
@@ -0,0 +1,13 @@
+const{test,expect}=require('@playwright/test')
+
+test('Login', async ({page})=>{
+     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+     await expect(page.locator('h5[class*="orangehrm-login-title"]')).toBeVisible();
+     await page.locator('input[name="username"]').fill('Admin');
+     await page.locator('input[type="password"]').fill('admin123');
+     await page.locator('button[type="submit"]').click();
+     await page.waitForTimeout(3000);
+     await expect(page.locator('a[href*="orangehrm.com"][class="oxd-brand"]')).toBeVisible();
+
+     
+})
---
+++ tests/test03.spec.js
@@ -0,0 +1,19 @@
+const{test,expect}=require('@playwright/test')
+
+test('View Leave Page', async ({page})=>{
+     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+     await expect(page.locator('h5[class*="orangehrm-login-title"]')).toBeVisible();
+     await page.locator('input[name="username"]').fill('Admin');
+     await page.locator('input[type="password"]').fill('admin123');
+     await page.locator('button[type="submit"]').click();
+     await page.waitForTimeout(3000);
+     await expect(page.locator('a[href*="orangehrm.com"][class="oxd-brand"]')).toBeVisible();
+
+     await page.locator('a[href="/web/index.php/leave/viewLeaveModule"]').click();
+     await page.waitForTimeout(3000);
+     await expect(page.locator('h6[class*="topbar"]')).toHaveText('Leave');
+
+
+
+     
+})
